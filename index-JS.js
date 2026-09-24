d1f0e7fc5ed5b8f47857a0d09636e58fb4481b2b
rhit-kingej1 <kingej1@rose-hulman.edu>
2026-09-24 13:46:36 -0400

    added some java script to button

---
+++ index-JS.js
@@ -0,0 +1,5 @@
+let resumeButton = document.getElementById("resume")
+resumeButton.addEventListener("click", function(){
+    console.log("button clicked")
+    this.onsecuritypolicyviolation.href = "resume.html";
+})
