e43e24e5952e97001782a39f2d88bbf2cadb9c60
rhit-kingej1 <kingej1@rose-hulman.edu>
2026-09-24 18:40:32 -0400

    added basic javascript

--- index-JS.js
+++
@@ -1,5 +0,0 @@
-let resumeButton = document.getElementById("resume")
-resumeButton.addEventListener("click", function(){
-    console.log("button clicked")
-    this.onsecuritypolicyviolation.href = "resume.html";
-})
---
+++ portfolio-JS.js
@@ -0,0 +1,17 @@
+// Portfolio's javascript
+
+window.addEventListener("DOMContentLoaded", domLoaded);
+let header = document.querySelector("header");
+let footer = document.querySelector("footer");
+
+function domLoaded() {
+    header.innerHTML = 
+            `<h3>Evelyn King</h3>
+            <nav>
+                <a id="portfolio-home" href="/index.html">Home</a>
+                <a id="portfolio-resume" href="/resume.html">Resume</a>
+            </nav>`;
+    
+    footer.innerHTML = '<a href="https://app.joinhandshake.com/profiles/y8kxvs">Handshake Profile</a>'
+
+}
